58de950cead9f4af8038a2e689310fb37244d097
hamid <mansouri.hamid23@gmail.com>
2025-11-16 19:02:14 +0100

    test: database

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TestRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(TestRepository $testRepository): Response
     {
-        return $this->render('home/home.html.twig');
+        return $this->render('home/home.html.twig', [
+            'tests' => $testRepository->findAll(),
+        ]);
     }
 }
